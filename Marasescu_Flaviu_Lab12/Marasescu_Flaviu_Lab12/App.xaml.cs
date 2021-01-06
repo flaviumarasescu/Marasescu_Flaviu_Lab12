11aa2e7e9882ec2f4cb7653ceb5f41576889bed1
FLAVIU-ȘTEFAN MĂRĂȘESCU <flaviu.marasescu@stud.ubbcluj.ro>
2021-01-06 13:17:15 +0200

    Add project files.

---
+++ Marasescu_Flaviu_Lab12/Marasescu_Flaviu_Lab12/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Marasescu_Flaviu_Lab12.Data;
+
+namespace Marasescu_Flaviu_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            //  InitializeComponent();
+
+            //  MainPage = new MainPage();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
